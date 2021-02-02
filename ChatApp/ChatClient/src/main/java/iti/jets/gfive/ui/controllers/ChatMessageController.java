683cc417343cba5a1b25667e3ec02111d77e5c02
MahaMekdad <maha_mekdad@outlook.com>
2021-02-02 22:13:41 +0200

    Adding ChatAreaView & ChatMsgView plus controllers and css

---
+++ ChatApp/ChatClient/src/main/java/iti/jets/gfive/ui/controllers/ChatMessageController.java
@@ -0,0 +1,13 @@
+package iti.jets.gfive.ui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ChatMessageController {
+    @FXML
+    private ImageView msgImgId;
+
+    @FXML
+    private Label msgLabelId;
+}
