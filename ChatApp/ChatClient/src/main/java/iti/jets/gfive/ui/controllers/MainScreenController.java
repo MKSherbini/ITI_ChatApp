14252df22cc8a871cfacb1314cf7e6b41ddc5e48
moHassan2525 <mohammed_hassan2525@yahoo.com>
2021-02-09 03:01:26 +0200

    adding the main screan to main screan branch and the icons

---
+++ ChatApp/ChatClient/src/main/java/iti/jets/gfive/ui/controllers/MainScreenController.java
@@ -0,0 +1,22 @@
+package iti.jets.gfive.ui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class  MainScreenController{
+
+@FXML
+private ImageView context_menu;
+
+@FXML
+    void showContextMenu(MouseEvent event) {
+
+    }
+
+@FXML
+    void showContxtMenu(MouseEvent event) {
+
+    }
+
+}
