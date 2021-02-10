7ee63f0b61f72a08caea7e74b8b50b07e25a230c
moHassan2525 <mohammed_hassan2525@yahoo.com>
2021-02-10 15:30:08 +0200

    adding the freind request dialog and its controller

---
+++ ChatApp/ChatClient/src/main/java/iti/jets/gfive/ui/controllers/FriendRequestDialog.java
@@ -0,0 +1,16 @@
+package iti.jets.gfive.ui.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FriendRequestDialog implements Initializable {
+
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
