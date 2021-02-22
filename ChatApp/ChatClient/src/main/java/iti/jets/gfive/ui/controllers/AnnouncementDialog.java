008ac691d5c26e71839e632c4cf6e0d039a07103
Mohammed Hassan <mohammed_hassan2525@yahoo.com>
2021-02-22 21:35:33 +0200

    added tested  ntificarion reaches the client

---
+++ ChatApp/ChatClient/src/main/java/iti/jets/gfive/ui/controllers/AnnouncementDialog.java
@@ -0,0 +1,20 @@
+package iti.jets.gfive.ui.controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.web.WebView;
+
+public class AnnouncementDialog {
+
+    @FXML
+    private WebView webView;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    void btnCancelClicked(ActionEvent event) {
+
+    }
+
+}
--- ChatApp/ChatServer/src/main/java/iti/jets/gfive/ui/controllers/AnnouncementView.java
+++ ChatApp/ChatServer/src/main/java/iti/jets/gfive/ui/controllers/AnnouncementView.java
@@ -1,11 +1,14 @@
 package iti.jets.gfive.ui.controllers;
 
+import iti.jets.gfive.server.ClientConnectionImpl;
+import iti.jets.gfive.ui.helpers.StageCoordinator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.web.HTMLEditor;
 
 import javax.swing.text.html.HTMLEditorKit;
+import java.rmi.RemoteException;
 
 public class AnnouncementView {
 
@@ -17,8 +20,17 @@ public class AnnouncementView {
 
     @FXML
     void sendAnnouncement(ActionEvent event) {
+        try {
+            ClientConnectionImpl clientConnection = new ClientConnectionImpl();
+            clientConnection.publishAnnouncement(htmlEditor.getHtmlText());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
         System.out.println(htmlEditor.getHtmlText());
     }
 
+    public void onBackBtn(ActionEvent actionEvent) {
+        StageCoordinator.getInstance().switchToServerMain();
+    }
 }
 
